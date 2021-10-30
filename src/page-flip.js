5f6856fe4fa3d5c4a97196e42420eefa16bcba86
Charles L <charles.lobo@gmail.com>
2021-10-30 14:25:58 +1100

    feat: add mouse click page flip

--- src/page-flip.js
+++ src/page-flip.js
@@ -15,18 +15,46 @@ export function init(id, pages) {
   rect.w /= 5
   rect.h /= 5
 
-  canvas.width = rect.w
+  canvas.width = rect.w * 2
   canvas.height = rect.h
 
-  const img = h("img", {
-    src: pages[0],
-    onload: () => {
-      ctx.drawImage(img, 0, 0, rect.w, rect.h)
-    },
+  canvas.attr({
+    onmousedown: evt => {
+      const pos = get_mouse_pos_1(canvas, evt)
+      if(pos.x < rect.w) {
+        page_1(0)
+        page_1(1)
+      } else {
+        page_1(2)
+        page_1(3)
+      }
+    }
   })
+
+  page_1(0)
+  page_1(1)
 
   e.c(
     canvas,
     toolbar,
   )
+
+  function page_1(ndx) {
+    const xoff = (ndx % 2) * rect.w
+    const img = h("img", {
+      src: pages[ndx],
+      onload: () => {
+        ctx.drawImage(img, xoff, 0, rect.w, rect.h)
+      },
+    })
+  }
+
+  function get_mouse_pos_1(canvas, evt) {
+    const rect = canvas.getBoundingClientRect()
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top,
+    }
+  }
+
 }
