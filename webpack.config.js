5c4184109be342b797bb85494e99be35dd724850
Charles L <charles.lobo@gmail.com>
2021-11-01 10:16:19 +1100

    feat: support dev/production builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,28 @@
 'use strict'
 const path = require('path')
 
-module.exports = {
+const base = {
   entry: './src/index.js',
   output: {
     filename: 'page-flip.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+}
 
+const dev = Object.assign({}, base, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
     static: './test',
   },
+})
+
+const prod = Object.assign({}, base, {
+  mode: "production",
+})
+
+module.exports = env => {
+  if(!env.production && env.development) return dev
+  else return prod
 }
