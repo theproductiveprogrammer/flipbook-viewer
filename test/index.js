7c4c46899803d2b4070864d34c31f3249214dc59
Charles L <charles.lobo@gmail.com>
2021-11-01 13:42:30 +1100

    feat: improve page generator api

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@
 
 import * as pageFlip from '../src/page-flip.js'
 
+import * as img from './imgpages.js'
 /*    understand/
  * main entry point into our program
  */
@@ -14,47 +15,12 @@ function main() {
     width: 800,
     height: 600,
   }
-  pageFlip.init(pageFn(), 'app', viewer => window.viewer = viewer)
-}
 
-function pageFn() {
-  const pages = [
-    '/000.png',
-    '/1.png',
-    '/2.png',
-    '/6.png',
-    '/3.png',
-    '/8.png',
-    '/9.png',
-    '/4.png',
-    '/5.png',
-    '/7.png',
-  ]
+  img.init((err, pageProvider) => {
+    if(err) console.error(err)
+    else pageFlip.init(pageProvider, 'app', viewer => window.viewer = viewer)
+  })
 
-  const imgs = []
-
-  return {
-    numPages: () => pages.length,
-    get,
-  }
-
-
-  function get(n, cb) {
-    if(!n || n > pages.length) return cb()
-    if(imgs[n]) return cb(null, imgs[n])
-
-    const img = new Image()
-    img.src = pages[n-1]
-    img.addEventListener("load", () => {
-      imgs[n] = {
-        img,
-        num: n,
-        width: img.width,
-        height: img.height,
-      }
-      cb(null, imgs[n])
-    }, false)
-  }
 }
 
 main()
